09141c4732d895987bddbe6fcbcf4d150185dcf4
albatroscom <albatroscom@naver.com>
2019-06-23 17:47:42 +0900

    Auth Form with Hooks

--- src/Routes/Auth.js
+++ src/Routes/Auth.js
@@ -2,6 +2,7 @@ import React, { useState } from 'react';
 import styled from 'styled-components';
 import Input from '../Components/Input';
 import Button from '../Components/Button';
+import useInput from '../Hooks/useInput';
 
 const Wrapper = styled.div`
     min-height: 80vh;
@@ -47,6 +48,17 @@ const Form = styled(Box)`
 
 export default () => {
     const [ action, setAction ] = useState('logIn');
+    const userName = useInput("");
+    const password = useInput("");
+    const firstName = useInput("");
+    const lastName = useInput("");
+    const email = useInput("");
+
+    console.log( userName,
+        password,
+        firstName,
+        lastName,
+        email );
 
     return (
         <Wrapper>
@@ -54,17 +66,17 @@ export default () => {
             <Form>
                 {action === 'logIn' ? (
                     <form>
-                        <Input placeholder={"Username"} />
-                        <Input placeholder={"Password"} />
+                        <Input placeholder={"Username"} {...userName} />
+                        <Input placeholder={"Password"} {...password} type="password" />
                         <Button text={"Log in"} />
                     </form>
                 ) : (
                     <form>
-                        <Input placeholder={"First name"} />
-                        <Input placeholder={"Last name"} />
-                        <Input placeholder={"Email"} />
-                        <Input placeholder={"Username"} />
-                        <Input placeholder={"Password"} />
+                        <Input placeholder={"First name"} {...firstName} />
+                        <Input placeholder={"Last name"} {...lastName} />
+                        <Input placeholder={"Email"} {...email} type="email" />
+                        <Input placeholder={"Username"} {...userName} />
+                        <Input placeholder={"Password"} {...password} type="password" />
                         <Button text={"Sign up"} />
                     </form>
                 )}
